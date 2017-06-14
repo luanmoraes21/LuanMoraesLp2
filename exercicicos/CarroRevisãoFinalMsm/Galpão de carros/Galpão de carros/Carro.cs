1288e468e8eab466ba363e0dbfce5f2f22aead48
Luan Moraes <luanmpbarros@gmail.com>
2017-06-13 22:22:16 -0300

    depois de 500 vezes acho que agora foi

---
+++ exercicicos/CarroRevisãoFinalMsm/Galpão de carros/Galpão de carros/Carro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galpão_de_carros
+{
+        class Carro
+        {
+        public string modelo;
+        public double quilometragem;
+        public double potencia, novo;
+        public int ano;
+      
+        public  string classificar()
+        {
+            string classe, potencial;
+
+            if (quilometragem <= 5000)
+                classe = "novo";
+
+            else if (quilometragem > 5000 && quilometragem < 30000)
+                classe = "seminovo";
+
+            else
+                classe = "velho";
+
+            if (potencia < 120)
+                potencial = "popular";
+
+            else if (potencia >= 120 && potencia <= 200)
+
+                potencial = "forte";
+
+            else
+                potencial = "poteeeente";
+
+            return string.Format("seu carro é um {0}-{1}-{2}", modelo, classe, potencial);
+}
+     }
+}
---
+++ exercicicos/CarroRevisãoFinalMsm/Galpão de carros/Galpão de carros/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galpão_de_carros
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("quantos carros são?");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Carro[] luan = new Carro[n];
+
+            
+            for (int i = 0; i < n; i++)
+
+            {
+                luan[i] = new Carro();
+
+                Console.WriteLine("\nInforme o modelo do carro");
+                luan[i].modelo = Console.ReadLine();
+
+                Console.WriteLine("\nInforme o número de km rodados");
+                luan[i].quilometragem = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInforme a potência do motor");
+                luan[i].potencia = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInforme o ano de fabricação");
+                luan[i].ano = int.Parse(Console.ReadLine());        
+            }
+       
+            
+            for (int i = 0; i < n; i++)
+           
+            {
+                Console.WriteLine(luan[i].Classificar());
+            }
+         }          
+    }         
+}
+
+
+
+    
+
