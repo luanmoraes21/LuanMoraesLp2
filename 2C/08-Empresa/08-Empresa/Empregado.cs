39bc7f5a35400ae7b0b700dc31e3cd9bda22b8a4
luan <luanmpbarros@gmail.com>
2017-09-06 18:01:00 -0300

    exercicio 8 o inicio

---
+++ 2C/08-Empresa/08-Empresa/Empregado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Empresa
+{
+    class Empregado : Funcionário
+    {
+        public int DataEntrada { get; set; }
+        public Gerente preda { get; set; }
+    }
+}
