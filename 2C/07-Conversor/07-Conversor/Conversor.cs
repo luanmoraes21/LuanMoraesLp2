9470e82416b76879a4f303f47057d977f106339f
Luan Moraes <luanmpbarros@gmail.com>
2017-09-22 17:26:06 -0300

    exercício 07-Conversor reajustado e finalizado

---
+++ 2C/07-Conversor/07-Conversor/Conversor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Conversor
+{
+    public static class Conversor
+    {
+            public static double KgParaG(double kg)
+            {
+                return kg * 1000;
+            }
+
+            public static double GParaKg(double g)
+            {
+                return g / 1000;
+            }
+
+            public static double KgParaT(double kg)
+            {
+                return kg / 1000;
+            }
+
+            public static double TParaKg(double t)
+            {
+                return t * 1000;
+            }
+
+            public static double LbsParaKg(double lbs)
+            {
+                return lbs / 2.20462;
+            }
+
+            public static double KgParaLbs(double kg)
+            {
+                return kg * 2.20462;
+            }
+
+            public static double CparaK(double c)
+            {
+                return c + 273;
+            }
+
+            public static double KparaC(double k)
+            {
+                return k - 273;
+            }
+
+            public static double FparaC(double f)
+            {
+                return (f - 32) / 9 * 5;
+            }
+
+            public static double CparaF(double c)
+            {
+                return c / 5 * 9 + 32;
+            }
+
+            public static double FparaK(double f)
+            {
+                return CparaK(FparaC(f));
+            }
+
+            public static double KparaF(double k)
+            {
+                return CparaF(KparaC(k));
+            }
+
+            public static double MParaKm(double m)
+            {
+                return m / 1000;
+            }
+
+            public static double KmParaM(double km)
+            {
+                return km * 1000;
+            }
+
+            public static double KmParaMilha(double km)
+            {
+                return km * 0.621371;
+            }
+
+            public static double MilhaParaKm(double milha)
+            {
+                return milha / 0.621371;
+            }
+
+            public static double PesParaM(double pes)
+            {
+                return pes / 3.28084;
+            }
+
+            public static double MParaPes(double m)
+            {
+                return m * 3.28084;
+            }
+
+            public static double MParaPol(double m)
+            {
+                return m * 39.3701;
+            }
+
+            public static double PolParaM(double pol)
+            {
+                return pol / 39.3701;
+            }
+
+            public static double PolParaPes(double pol)
+            {
+                return MParaPes(PolParaM(pol));
+            }
+
+            public static double PesParaPol(double pes)
+            {
+                return MParaPol(PesParaM(pes));
+            }
+        }
+    }
+
+    
+    
+
---
+++ 2C/07-Conversor/07-Conversor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolheu = -1;
+
+            while (escolheu != 0)
+            {
+                Console.WriteLine("Qual tipo de conversão será utilizado? ");
+
+                Console.WriteLine("1- Kg > g");
+                Console.WriteLine("2- g > Kg");
+                Console.WriteLine("3- Kg > T");
+                Console.WriteLine("4- T > Kg");
+                Console.WriteLine("5- °C > °F");
+                Console.WriteLine("6- °F > °C");
+                Console.WriteLine("7- °K > °F");
+
+                escolheu = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o valor a ser convertido: ");
+                double valnum = double.Parse(Console.ReadLine());
+
+                switch (escolheu)
+                {
+                    case 1:
+                        Console.WriteLine("{0:0.0} g", Conversor.KgParaG(valnum));
+                        break;
+                    case 2:
+                        Console.WriteLine("{0:0.0} kg", Conversor.GParaKg(valnum));
+                        break;
+                    case 3:
+                        Console.WriteLine("{0:0.0} T", Conversor.KgParaT(valnum));
+                        break;
+                    case 4:
+                        Console.WriteLine("{0:0.0} kg", Conversor.TParaKg(valnum));
+                        break;
+                    case 5:
+                        Console.WriteLine("{0:0.0} ºF", Conversor.CparaF(valnum));
+                        break;
+                    case 6:
+                        Console.WriteLine("{0:0.0} ºC", Conversor.FparaC(valnum));
+                        break;
+                    case 7:
+                        Console.WriteLine("{0:0.0} ºC", Conversor.KparaF(valnum));
+                        break;
+        }
+    }
+}
